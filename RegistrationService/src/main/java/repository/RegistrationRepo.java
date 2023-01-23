9a1808454874da8d17639ddb2da8653ecc7b78e9
navjotNSK <118380435+navjotNSK@users.noreply.github.com>
2023-01-23 20:16:35 +0530

    Update RegistrationRepo.java

--- RegistrationService/src/main/java/repository/RegistrationRepo.java
+++ RegistrationService/src/main/java/repository/RegistrationRepo.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface RegistrationRepo {
 
-	public boolean addUser(String userName , long phoneNo , String emailId , String userPassword , String userRole , String techStack , String jobRole );
+	public boolean addUser(String userName , long phoneNo , String emailId , String userPassword , String userRole , String techStack , String jobRole )  ;
 	public boolean updateUser(String userName , long phoneNo , String emailId , String userPassword , String userRole , String techStack , String jobRole);
 	public boolean deleteUser(String emailId );
 	public List<String> getUserByMail(String emailId);
